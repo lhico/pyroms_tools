15d440e4b863ddd810e4584d8f1d527ff9525fca
dksasaki <dalton.sasaki@gmail.com>
2022-11-07 16:47:23 -0300

    extending interpolation to u and v variables

--- scripts_xesmf/make_ic_file_replace.py
+++ scripts_xesmf/make_ic_file_replace.py
@@ -83,6 +83,60 @@ def interpolation(fpath, nc_roms_grd, source_grid, target_grid, gridtype='rho'):
     return interpvarb
 
 
+def interpolation2d(fpath, nc_roms_grd, source_grid, target_grid, gridtype='rho'):
+    target_grid = target_grid.copy()
+
+    coords_rename = {'roms2data':
+                        {
+                            'rho': {'lon_rho':'lon', 'lat_rho':'lat'},
+                            'u'  : {'lon_u':'lon', 'lat_u':'lat'},
+                            'v'  : {'lon_v':'lon', 'lat_v':'lat'},
+                        },
+                     'data2roms':
+                       {
+                            'rho': {'lon':'lon_rho', 'lat':'lat_rho'},
+                            'u'  : {'lon':'lon_u', 'lat':'lat_u'},
+                            'v'  : {'lon':'lon_v', 'lat':'lat_v'},
+                       }
+
+                    }
+
+    # TODO fix up the vertical axis
+    nc0 = xr.open_dataset(fpath)
+
+    nc_roms_grd = nc_roms_grd.assign_coords(s_rho=nc0.s_rho)
+    nc_roms_grd = nc_roms_grd.assign_coords(s_w = nc0.s_w)
+    nc_roms_grd.Cs_r.values = nc0.Cs_r.values
+    nc_roms_grd.hc.values = nc0.hc.values
+    nc_roms_grd.Cs_w.values = nc0.Cs_w.values
+
+    target_grid = target_grid.rename(coords_rename['roms2data'][gridtype])
+
+    # define vertical coordinates
+    z,_ = compute_depth_layers(nc_roms_grd) 
+    z = z.transpose(*('s_rho','eta_rho','xi_rho'), transpose_coords=False)
+    z = z.values
+
+
+    #  horizontal interpolation
+    regridder = xe.Regridder(source_grid, target_grid, 'bilinear', extrap_method='nearest_s2d')
+    interpolated = regridder(source_grid)  # interpolating
+
+    interpvarb = interpolated.values
+
+    # mask = nc_roms_grd[f'mask_{gridtype}'].values
+    # ind = np.where(mask!=0)
+
+    # for j,i in zip(ind[0], ind[1]):
+    #     print(j,i, end='\r')
+    #     f = interpolate.interp1d(-interpolated.z.values,
+    #                               interpolated[:,j,i].values,
+    #                               bounds_error=False,
+    #                               fill_value='extrapolate',
+    #                               kind='slinear')
+    #     interpvarb[:,j,i] = f(z[::,j,i])
+    return interpvarb
+
 def extrapolation_nearest(x,y,var, maskvalue=None):
     "nearest-neighbor extrapolation with cKDTree method"
     
@@ -132,6 +186,69 @@ def extrapolation_nearest(x,y,var, maskvalue=None):
 
 
 
+def rotate_vector_field(nc):
+    """
+    Rotates vector fields in roms ic file.
+    1) regrid variables in u and v coordinates to rho coordinates
+    2) rotate vectors
+    3) regridded and rotated variables are  transformed back to u and v coordinates
+
+
+    Args:
+        nc (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    nc_out = nc.copy()
+    nc_out.load()
+
+    # -- regrid variables in u and v coordinates to rho coordinates -- #
+    target_grid = nc_out.rename({'lon_rho': 'lon', 'lat_rho':'lat'})
+    source_grid = nc_out.rename({'lon_u': 'lon', 'lat_u':'lat'})
+    
+    #  horizontal interpolation
+    regridder = xe.Regridder(source_grid, target_grid, 'bilinear', extrap_method='nearest_s2d')
+    interp_u = regridder(source_grid)  # interpolating
+    
+    target_grid = nc_out.rename({'lon_rho': 'lon', 'lat_rho':'lat'})
+    source_grid = nc_out.rename({'lon_v': 'lon', 'lat_v':'lat'})
+    #  horizontal interpolation
+    regridder = xe.Regridder(source_grid, target_grid, 'bilinear', extrap_method='nearest_s2d')
+    interp_v = regridder(source_grid)  # interpolating
+
+    # -- rotate vectors -- #
+    rot = nc_out.angle
+    for var in [['u','v'],['ubar','vbar']]:
+        u = interp_u[var[0]]
+        v = interp_v[var[1]]
+
+        mag = (u**2 + v**2)**0.5
+        angle = np.arctan2(v,u)
+
+        u1 = -mag * np.sin(angle + rot)
+        v1 = mag * np.cos(angle + rot)
+        interp_u[var[0]] = u1
+        interp_v[var[1]] = v1
+
+    # -- regridded and rotated variables are  transformed back to u and v coordinates -- #
+    target_grid = nc_out.rename({'lon_u': 'lon', 'lat_u':'lat'})
+    source_grid = interp_u
+    regridder = xe.Regridder(source_grid, target_grid, 'bilinear', extrap_method='nearest_s2d')
+    regrid_u  = regridder(source_grid)
+
+    target_grid = nc_out.rename({'lon_v': 'lon', 'lat_v':'lat'})
+    source_grid = interp_v
+    regridder = xe.Regridder(source_grid, target_grid, 'bilinear', extrap_method='nearest_s2d')
+    regrid_v  = regridder(source_grid)
+
+    for var in [['u','v'],['ubar','vbar']]:
+        nc_out[var[0]]= regrid_u[var[0]]
+        nc_out[var[1]]= regrid_v[var[1]]
+
+    return nc_out
+
+
 
 if __name__ == '__main__':
 
@@ -140,6 +257,8 @@ if __name__ == '__main__':
     horizonta_homog_fields = False
 
     reference = 'pbs_202109_glorys'
+  
+
 
     dicts = ut._get_dict_paths('../configs/grid_config_esmf.txt')
     dicts = dicts[reference]
@@ -161,6 +280,7 @@ if __name__ == '__main__':
 
     nc_ini_src = nc_ini_src.rename_dims(rename_coords)
     nc_ini_src = nc_ini_src.rename_vars(rename_vars)
+    print(nc_ini_src.time.values)
     # nc_ini_src = nc_ini_src.assign_coords(z=nc_ini_src.z)
 
     
@@ -177,7 +297,7 @@ if __name__ == '__main__':
 
 
     # 2) linear interpolation from source to roms grid (horizontal) -> roms_aux
-    # the extrapolation procedure is not generic!!
+    # salinity and temperature must have 4 dimension (time, depth, lat,)
     for varb in varbs:
 
         print(f'\n# -- Interpolating {varb} --#\n')
@@ -189,27 +309,37 @@ if __name__ == '__main__':
             gtype = 'v' 
         
         print(f'grid type: {gtype}')
+        print(f'{varb} shape: {nc_ini_src[varb].values.ndim}')
         
+        if "time" not in nc_ini_src[varb].coords:
+            raise IOError("time should be a coordinate")
+
+
         if nc_ini_src[varb].values.ndim == 4:
             nc_ini_src[varb].values[0] = extrapolation_nearest(nc_ini_src.longitude.values,
                                                             nc_ini_src.latitude.values,
                                                             nc_ini_src[varb].values[0,])
             var = interpolation(dicts['grid_dir'], nc_roms_grd, nc_ini_src[varb][0], ds_out, gridtype=gtype)
+
         elif nc_ini_src[varb].values.ndim == 3:
+            shp = nc_ini_src[varb].values.shape
             nc_ini_src[varb].values = extrapolation_nearest(nc_ini_src.longitude.values,
                                                             nc_ini_src.latitude.values,
                                                             nc_ini_src[varb].values)
-            var = interpolation(dicts['grid_dir'], nc_roms_grd, nc_ini_src[varb], ds_out, gridtype=gtype)
+            var = interpolation2d(dicts['grid_dir'], nc_roms_grd, nc_ini_src[varb], ds_out, gridtype=gtype)
+            var = var.squeeze()
         else:
             raise IndexError('array should be either 3D or 4D')
         nc_out[varb].values = [var]
 
+    nc_out1 = rotate_vector_field(nc_out)
 
-    nc_out.u.values[:] = 0 
-    nc_out.v.values[:] = 0 
-    nc_out.ubar.values[:] = 0 
-    nc_out.vbar.values[:] = 0 
+    
+    # nc_out.u.values[:] = 0 
+    # nc_out.v.values[:] = 0 
+    nc_out1.ubar.values[:] = ((nc_out1.u * nc_out1.s_rho).sum(dim='s_rho') / nc_out1.s_rho.sum()).values
+    nc_out1.vbar.values[:] = ((nc_out1.v * nc_out1.s_rho).sum(dim='s_rho') / nc_out1.s_rho.sum()).values
 
     os.system(f'rm {outfile}')
-    nc_out.to_netcdf(outfile)
+    nc_out1.to_netcdf(outfile)
 
